d1d3bc4447af643fb410199d055afadaf9d3cd3d
Utkarshaanand <utkarshaanand123@gmail.com>
2019-05-01 17:53:28 +0530

    Final commit

---
+++ server/utils/message.test.js
@@ -0,0 +1,27 @@
+var expect = require('expect');
+
+var {generateMessage, generateLocationMessage} = require('./message');
+
+describe('generateMessage', () => {
+  it('should generate correct message object', () => {
+    var from = 'Hazard';
+    var text = 'Real Madrid';
+    var message = generateMessage(from, text);
+
+    expect(message).toInclude({from, text});
+    expect(message.createdAt).toBeA('number');
+  })
+})
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Ronaldo';
+    var latitude = 15;
+    var longitude = 19;
+    var url = "https://www.google.com/maps?q=15,19";
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    expect(message).toInclude({from, url});
+    expect(message.createdAt).toBeA('number');
+  })
+})
